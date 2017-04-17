159ed142d6e9fbeb4c48b3c05544e087bdb6b073
katzelda <katzelda@mail.nih.gov>
2017-04-17 10:10:40 -0400

    added -insecure flag to get around jenkins https issue

--- downloadDrugsAtFda.sh
+++ downloadDrugsAtFda.sh
@@ -1,1 +1,1 @@
-curl https://www.fda.gov/Drugs/InformationOnDrugs/ucm079750.htm | grep "Drugs@FDA Download File\&nbsp"| perl -ne 'print "https://www.fda.gov.$1" if /href=\"(\S+)\"/' | xargs curl -o drugsAtfda.zip
+curl -insecure https://www.fda.gov/Drugs/InformationOnDrugs/ucm079750.htm | grep "Drugs@FDA Download File\&nbsp"| perl -ne 'print "https://www.fda.gov.$1" if /href=\"(\S+)\"/' | xargs curl -insecure -o drugsAtfda.zip
