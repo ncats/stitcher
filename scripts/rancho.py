14470056eaa726316e9c8e330ad127063b2eeaff
grishagin <grishagin@gmail.com>
2017-09-09 20:22:24 -0400

    changed rancho.py to fix issues with missing conditions/targets

--- scripts/rancho.py
+++ scripts/rancho.py
@@ -1,7 +1,6 @@
 import json
 import sys
 import hashlib
-
 conditions = [
     'ConditionName',
     'ConditionUri',
@@ -51,49 +50,69 @@ def rancho (file):
     with open (file, 'rb') as file:
         reader = csv.reader(file,dialect='excel-tab')
         header = reader.next()
-        line = 0
-        p = None
+        #line counter for debugging purposes
+        line = 1
+        #declare a "summary object" for one compound
+        combined_comp_rows = None
         print '[',
         for row in reader:
-            #print '--- record {0} ---'.format(line)
-            d = {}
+            line += 1
+            #dictionary to store current row info
+            current_row_dict = {}
             c = {} # condition
             t = {} # target
-            hc = hashlib.new('sha1')
-            ht = hashlib.new('sha1')
+            hc = hashlib.sha1()
+            ht = hashlib.sha1()
+
+            #dissect the row
             for i in range(0,len(header)):
+                #get the variable/column name
                 name = header[i]
-                    
+
+                #some rows are shorter than header
                 if i < len(row):
+                    #get rid of white space around each element
                     r = row[i].strip()
+
+                    #if the row element is non-empty
                     if r != '':
-                        x = d
+
+                        #point temp variable to row dictionary
+                        x = current_row_dict
+
+                        #if col name belongs to conditions
                         if name in conditions:
+                            #point temp var to conditions
                             x = c
+                            #make a hash
                             hc.update(name)
+                        #likewise, for targets
                         elif name in targets:
                             x = t
                             ht.update(name)
-                        
+
+                        #split up the observations
                         vals = r.split('|')
+
+                        #cycle through all observations
                         if len(vals) > 1:
-                            numeric = []
-                            others = []
-                            for v in vals:
-                                v = v.strip()
-                                if v != '':
+                            for vi in range(len(vals)):
+                                #remove trailing whitespaces
+                                vals[vi] = vals[vi].strip()
+
+                                if vals[vi] != '':
+                                    #convert to digit
+                                    if vals[vi].isdigit():
+                                        vals[vi] = long(vals[vi])
+
+                                    #update targets'/conditions' hashes
                                     if name in conditions:
                                         hc.update(v)
                                     elif name in targets:
                                         ht.update(v)
-                                    if v.isdigit():
-                                        numeric.append(long(v))
-                                    else:
-                                        others.append(v)
-                            if len(numeric) == len(vals):
-                                x[name] = numeric
-                            else:
-                                x[name] = others
+
+                            #add value to corresponding variable in temp row object
+                            x[name] = vals
                         elif name == 'Unii':
                             unii = []
                             for v in r.split(';'):
@@ -111,68 +130,106 @@ def rancho (file):
                                 x[name] = long(r)
                             elif r.lower() != 'null' and r.lower() != 'unknown':
                                 x[name] = r
-                                
+
+                            #update the hashes for conditions/targets
+                            #if applicable
                             if name in conditions:
                                 hc.update(r)
                             elif name in targets:
                                 ht.update(r)
 
-            if not d.has_key('CompoundName'):
+            if not current_row_dict.has_key('CompoundName'):
                 sys.stderr.write(('{0}: No CompoundName: '+'|'.join(row)+'\n').format(line))
                 continue
             
             if c.has_key('ConditionName') or c.has_key('ConditionMeshValue') or c.has_key('ConditionDoValue'):
                 c['id'] = hc.hexdigest()[0:10]
-                d['Conditions'] = [c]
+                current_row_dict['Conditions'] = [c]
+
             if t.has_key('PrimaryTargetId'):
                 t['id'] = ht.hexdigest()[0:10]
-                d['Targets'] = [t]
-                
-            if d['Connected'] and len(t) > 0:
+                current_row_dict['Targets'] = [t]
+
+            if current_row_dict['Connected'] and len(t) > 0:
                 c['TargetRefs'] = [t['id']]
                 t['ConditionRefs'] = [c['id']]
 
-            del d['Connected']
-            #print json.dumps(d, indent=4, separators=(',',': '))
-            
-            if p != None:
-                if p['CompoundName'] == d['CompoundName']:
-                    if p.has_key('Conditions') and c.has_key('id'):
-                        for pc in p['Conditions']:
-                            if pc['id'] == c['id']:
-                                if c.has_key('TargetRefs'):
-                                    if pc.has_key('TargetRefs'):
-                                        pc['TargetRefs'].append(c['TargetRefs'][0])
-                                    else:
-                                        pc['TargetRefs'] = c['TargetRefs']
-                                c = None
-                                break
-                        if c != None:
-                            p['Conditions'].append(c)
-                            
-                    if p.has_key('Targets') and t.has_key('id'):
-                        for pt in p['Targets']:
-                            if pt['id'] == t['id']:
-                                if t.has_key('ConditionRefs'):
-                                    if pt.has_key('ConditionRefs'):
-                                        pt['ConditionRefs'].append(t['ConditionRefs'][0])
-                                    else:
-                                        pt['ConditionRefs'] = t['ConditionRefs']
-                                t = None
-                                break
-                        if t != None:
-                            p['Targets'].append(t)
-                    d = p
-                else:
-                    print json.dumps(p, indent=4, separators=(',',': '))+','
+            #remove connected key (already used)
+            del current_row_dict['Connected']
+            #and curated by (not needed)
+            if current_row_dict.has_key('CuratedBy'):
+                del current_row_dict['CuratedBy']
 
-            line = line + 1
-            p = d
-            
-        if p != None:
-            print json.dumps(p, indent=4, separators=(',',': '))
-        print ']'
-            
+            #if compound summary object does not exist...
+            if combined_comp_rows == None:
+                #point it to current row object
+                combined_comp_rows = current_row_dict
+            else:
+                #if it exists and does not belong to the same compound as row object...
+                if combined_comp_rows['CompoundName'] != current_row_dict['CompoundName']:
+                    #dump json to file
+                    print json.dumps(combined_comp_rows, indent=4, separators=(',', ': ')) + ','
+                    #and point the summary object to new row
+                    combined_comp_rows = current_row_dict
+                else:
+                    #if new row has information on the compound in the summary
+                    #if there's a condition key 'id'...
+                    if c.has_key('id'):
+                        #and that key exists in the summary object...
+                        if combined_comp_rows.has_key('Conditions'):
+                            #append all target references from that key
+                            #to every condition with the same id...
+                            for pc in combined_comp_rows['Conditions']:
+                                if pc['id'] == c['id']:
+                                    if c.has_key('TargetRefs'):
+                                        if pc.has_key('TargetRefs'):
+                                            #[0] is used because that property
+                                            # was deliberately made a list
+                                            pc['TargetRefs'].append(c['TargetRefs'][0])
+                                        else:
+                                            pc['TargetRefs'] = c['TargetRefs']
+                                    c = None
+                                    break
+                            else:
+                                #and finally append the new condition
+                                combined_comp_rows['Conditions'].append(c)
+                        #if it's the first condition -- create it in the summary object anew
+                        else:
+                            combined_comp_rows['Conditions'] = [c]
+
+                    #do the same thing for targets
+                    #if there's a target key 'id'...
+                    if t.has_key('id'):
+                        #and that key exists in the summary object...
+                        if combined_comp_rows.has_key('Targets'):
+                            #append all condition references from that key
+                            #to every target with the same id as current target...
+                            for pt in combined_comp_rows['Targets']:
+                                if pt['id'] == t['id']:
+                                    if t.has_key('TargetRefs'):
+                                        if pt.has_key('TargetRefs'):
+                                            #[0] is used because that property
+                                            # was deliberately made a list
+                                            pt['TargetRefs'].append(t['TargetRefs'][0])
+                                        else:
+                                            pt['TargetRefs'] = t['TargetRefs']
+                                    t = None
+                                    break
+                            else:
+                                #and finally append the new target
+                                combined_comp_rows['Targets'].append(t)
+                        #if it's the first target -- create it in the summary object anew
+                        else:
+                            combined_comp_rows['Targets'] = [t]
+                    #point current row object to summary object
+                    current_row_dict = combined_comp_rows
+
+    #in the end either a new row or a summary object point to the same unrecorded object
+    #so it needs to be recorded
+    print json.dumps(current_row_dict, indent=4, separators=(',',': '))
+    print ']'
+
+
 if __name__ == "__main__":
     import sys
     rancho(sys.argv[1])
