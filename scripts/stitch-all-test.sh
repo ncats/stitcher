bcced7c54c0243daeefdd6b2bda18295f3e25bb6
grishagin <grishagin@gmail.com>
2018-01-05 18:21:43 -0500

    initial broad-related files

---
+++ scripts/stitch-all-test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+db="stitchvBROAD.db"
+
+#keep track of current time
+curr_time=$(date +%s)
+
+echo $(date) > log.txt
+
+sbt stitcher/"runMain ncats.stitcher.impl.MapEntityFactory $db data/broad.conf"
+sbt stitcher/"runMain ncats.stitcher.tools.CompoundStitcher $db 1"
+sbt stitcher/"runMain ncats.stitcher.calculators.EventCalculator $db 1"
+
+echo $(date) >> log.txt
