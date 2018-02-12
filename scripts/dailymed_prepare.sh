d7577351cce7debb165172de06a3664d9f84e0b9
grishagin <grishagin@gmail.com>
2018-02-12 02:25:48 -0500

    Modified SPL parser to record additional variables. Added new SPL files. Split SPL parser wrapper into two scripts (get and prepare).

---
+++ scripts/dailymed_prepare.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+#run this from stitcher directory!
+if [[ ! `pwd` == */stitcher ]]; then
+	echo "Run this script from the main stitcher directory! Aborting."
+	exit 1
+fi
+
+######################################## declare/check ########################################
+
+#declare a directory where the original files are located
+#ATTN: need '/' at the end!
+save_to=../
+
+#declare all necessary files
+files=(
+		dm_spl_release_human_rx_part1.zip 
+		dm_spl_release_human_rx_part2.zip 
+		dm_spl_release_human_rx_part3.zip
+		dm_spl_release_human_otc_part1.zip 
+		dm_spl_release_human_otc_part2.zip 
+		dm_spl_release_human_otc_part3.zip 
+		dm_spl_release_human_otc_part4.zip 
+		dm_spl_release_human_otc_part5.zip
+		dm_spl_release_remainder.zip)
+		
+#append full paths to file names
+files=( "${files[@]/#/$save_to}" ) 
+
+missing_files=()
+
+#check if all files are there
+for f in ${files[@]}; do
+	if [ ! -f $f ]; then
+		echo "File $f is missing!"
+		missing_files+=($f)
+	fi
+done
+
+if [ ${#missing_files[@]} -gt 0 ]; then
+	echo "Some files are missing! Aborting."
+	exit 1
+fi
+
+######################################## parse/prepare ########################################
+echo "Now to parsing."
+types=(
+		_rx
+		_otc
+		_rem)
+
+for type in ${types[@]}; do
+	#select necessary files
+	subset=`printf '%s\n' "${files[@]}" | grep "$type"`
+	
+	echo "Parsing $subset files..."
+	
+	#parse them
+	sbt dailymed/"runMain ncats.stitcher.dailymed.DailyMedParser `echo $subset`" > spl$type.txt 
+	
+	wait
+		
+	#remove all lines starting with control elements (they are auxiliary)
+	sed -ie '/^[[:cntrl:]]/ d' spl$type.txt 
+	sed -ie '/^java/ d' spl$type.txt 
+	
+	#leave only active compounds (otherwise stitching later will take too long)
+	cat spl$type.txt | sed '/\tIACT\t/d' > spl_acti$type.txt
+	
+	#gzip the original file
+	gzip spl$type.txt 
+done
