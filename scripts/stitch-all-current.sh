512f4a6e123d3b7cd7e97ee90444e29189aff8c2
grishagin <grishagin@gmail.com>
2018-02-16 00:40:46 -0500

    Added new rancho tsv and json dumps. Modified main script accordingly.

--- scripts/stitch-all-current.sh
+++ scripts/stitch-all-current.sh
@@ -15,7 +15,7 @@ sbt stitcher/"runMain ncats.stitcher.impl.LineMoleculeEntityFactory $db data/rui
 echo 'Ruili:' $(( ($(date +%s) - $curr_time )/60 )) 'min' >> $log
 sbt stitcher/"runMain ncats.stitcher.impl.SRSJsonEntityFactory $db cache=data/hash.db data/dump-public-2018-02-06.gsrs"
 echo 'gsrs:' $(( ($(date +%s) - $curr_time )/60 )) 'min' >> $log
-sbt stitcher/"runMain ncats.stitcher.impl.RanchoJsonEntityFactory $db cache=data/hash.db data/rancho-export_2018-02-05_22-22.json"
+sbt stitcher/"runMain ncats.stitcher.impl.RanchoJsonEntityFactory $db cache=data/hash.db data/rancho-export_2018-02-16_06-27.json"
 echo 'rancho:' $(( ($(date +%s) - $curr_time )/60 )) 'min' >> $log
 sbt stitcher/"runMain ncats.stitcher.impl.NPCEntityFactory $db cache=data/hash.db ../inxight-planning/files/npc-dump-1.2-04-25-2012_annot.sdf.gz"
 echo 'NPC:' $(( ($(date +%s) - $curr_time )/60 )) 'min' >> $log
