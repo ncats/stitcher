42233ba061502a0da62e385cfbdf8abaebd47342
ivan.grishagin <ivan.grishagin@nih.gov>
2018-03-08 02:38:30 -0500

    Added 'Comment' field to SPL data and dev_status_logic, changed logic accordingly. Changed dev logic to account for 'ALLERGENIC EXTRACT' substances.

--- scripts/stitch-all-test.sh
+++ scripts/stitch-all-test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-db="stitchvBROAD.db"
+db="stitchvTEST.db"
 
 #keep track of current time
 curr_time=$(date +%s)
 
 echo $(date) > log.txt
 
-sbt stitcher/"runMain ncats.stitcher.impl.LineMoleculeEntityFactory $db data/broad.conf"
+sbt stitcher/"runMain ncats.stitcher.impl.SRSJsonEntityFactory $db \"name=G-SRS, February 2018\" cache=data/hash.db data/dump-public-2018-02-06-TEST.gsrs"
 sbt stitcher/"runMain ncats.stitcher.tools.CompoundStitcher $db 1"
 sbt stitcher/"runMain ncats.stitcher.calculators.EventCalculator $db 1"
 
