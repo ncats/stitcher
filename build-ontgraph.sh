77dbc00d3ba44de5512925adedc6deb9d93d6662
trung <caodac@gmail.com>
2018-12-19 19:31:32 -0500

    add hpo annotations to stitch disease & phenotype

--- build-ontgraph.sh
+++ build-ontgraph.sh
@@ -15,5 +15,12 @@ owl_path="owl"
 owl_files=`echo $owl | xargs printf " ${owl_path}/%s"`
 #echo $owl_files
 
-out="ncatsowl.db"
+out="ncatskg.db"
+# load ontologies
 sbt -Djdk.xml.entityExpansionLimit=0 stitcher/"runMain ncats.stitcher.impl.OntEntityFactory $out $owl_files"
+
+#load rancho
+sbt stitcher/"runMain ncats.stitcher.impl.InxightEntityFactory $out data/rancho-disease-drug_2018-12-18_13-30.txt"
+
+#load hpo annotations
+sbt stitcher/"runMain ncats.stitcher.impl.HPOEntityFactory $out data/HPO_annotation_100918.txt
