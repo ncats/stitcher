c121ce673d04f7b67aeb4c552bc82962bc54213d
trung <caodac@gmail.com>
2018-12-20 23:10:52 -0500

    label hpo annotations; refine chebi; update instance counts

--- modules/stitcher/src/main/java/ncats/stitcher/impl/MoleculeEntityFactory.java
+++ modules/stitcher/src/main/java/ncats/stitcher/impl/MoleculeEntityFactory.java
@@ -272,15 +272,14 @@ public class MoleculeEntityFactory extends EntityRegistry {
         return ds;
     }
 
-    public DataSource register (String key, String name, File file) throws IOException {
-        logger.warning("I'm MoleculeEntityFactory");
+    public DataSource register (String key, String name, File file)
+        throws IOException {
         DataSource ds = super.register(key, name, file);
         register (ds);
         return ds;
     }
 
     public DataSource register (String name, File file) throws IOException {
-        logger.warning("I'm MoleculeEntityFactory");
         DataSource ds = super.register(name, file);
         register (ds);
         return ds;
