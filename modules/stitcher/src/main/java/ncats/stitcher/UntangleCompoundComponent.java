cf32833172d497c68a2982364251e4f8deca705d
nsouthall <southalln@mail.nih.gov>
2020-07-18 21:38:58 -0400

    alter activeMoiety unsure logic

--- modules/stitcher/src/main/java/ncats/stitcher/UntangleCompoundComponent.java
+++ modules/stitcher/src/main/java/ncats/stitcher/UntangleCompoundComponent.java
@@ -395,17 +395,24 @@ public class UntangleCompoundComponent extends UntangleCompoundAbstract {
         // collapse based on single/terminal active moieties
         Set<Entity> unsure = new TreeSet<>();
         component.stitches((source, target) -> {
-                Entity[] out = source.outNeighbors(R_activeMoiety);
+                //Entity[] out = source.outNeighbors(R_activeMoiety);
+                Object moietyKeys = source.keys().get(R_activeMoiety);
+                int keyCount = 0;
+                if (moietyKeys.getClass().isArray())
+                    keyCount = Array.getLength(moietyKeys);
                 Entity[] in = target.inNeighbors(R_activeMoiety);
-                logger.info(" ("+out.length+") "+source.getId()
+                //logger.info(" ("+out.length+") "+source.getId()
+                logger.info(" ("+keyCount+") "+source.getId()
                             +" -> "+target.getId()+" ["
                             +isRoot (target)+"] ("+in.length+")");
                 
-                if (out.length == 1) {
+                //if (out.length == 1) {
+                if (keyCount == 1) {
                     // first collapse single/terminal active moieties
                     uf.union(target.getId(), source.getId());
                 }
-                else if (out.length > 1) {
+                //else if (out.length > 1) {
+                else if (keyCount > 1) {
                     unsure.add(source);
                 }
 
