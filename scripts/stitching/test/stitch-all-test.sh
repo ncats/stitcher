21e891720db9f0ba94a8b6474fe82f1f0a0551eb
Ivan Grishagin <grishagin@gmail.com>
2021-03-15 01:06:48 -0400

    Updated test stitching script.

--- scripts/stitching/test/stitch-all-test.sh
+++ scripts/stitching/test/stitch-all-test.sh
@@ -8,7 +8,7 @@ curr_time=$(date +%s)
 
 echo $(date) > $log
 
-sbt stitcher/"runMain ncats.stitcher.impl.RanchoJsonEntityFactory $db \"name=FRDB, February 2021\" cache=data/hash.db ../stitcher-rawinputs/files/frdb_2021-02-01_v1.json"
+sbt stitcher/"runMain ncats.stitcher.impl.RanchoJsonEntityFactory $db \"name=FRDB, February 2021\" cache=data/hash.db ../stitcher-rawinputs/files/frdb_2021-02-25.json"
 echo 'rancho:' $(( ($(date +%s) - $curr_time )/60 )) 'min' >> $log
 sbt stitcher/"runMain ncats.stitcher.impl.SRSJsonEntityFactory $db \"name=G-SRS, April 2020\" cache=data/hash.db ../stitcher-rawinputs/files/dump-public-2020-04-28.gsrs"
 echo 'gsrs:' $(( ($(date +%s) - $curr_time )/60 )) 'min' >> $log
@@ -16,5 +16,9 @@ echo 'gsrs:' $(( ($(date +%s) - $curr_time )/60 )) 'min' >> $log
 #sbt stitcher/"runMain ncats.stitcher.impl.MapEntityFactory stitchvTEST.db data/ob.conf"
 #sbt stitcher/"runMain ncats.stitcher.tools.CompoundStitcher $db 1"
 #sbt stitcher/"runMain ncats.stitcher.calculators.EventCalculator $db 1"
+
+# now the stitching...
+sbt -mem 32000 stitcher/"runMain ncats.stitcher.tools.CompoundStitcher $db 1"
+echo 'Stitching:' $(( ($(date +%s) - $curr_time )/60 )) 'min' >> $log
 
 echo $(date) >> $log
