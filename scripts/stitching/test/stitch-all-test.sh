80101b5771ae115b91b3266c59d3c5b9ba42a733
nsouthall <southalln@mail.nih.gov>
2021-05-21 14:10:51 -0400

    update gsrs dump to 2021 May

--- scripts/stitching/test/stitch-all-test.sh
+++ scripts/stitching/test/stitch-all-test.sh
@@ -7,7 +7,7 @@ curr_time=$(date +%s)
 
 echo $(date) > log.txt
 
-sbt stitcher/"runMain ncats.stitcher.impl.SRSJsonEntityFactory $db \"name=G-SRS, April 2020\" cache=data/hash.db ../stitcher-rawinputs/files/dump-public-2020-04-28.gsrs"
+sbt stitcher/"runMain ncats.stitcher.impl.SRSJsonEntityFactory $db \"name=G-SRS, May 2021\" cache=data/hash.db ../stitcher-rawinputs/files/dump-public-2021-05-02.gsrs"
 #sbt stitcher/"runMain ncats.stitcher.impl.MapEntityFactory stitchvTEST.db data/ob.conf"
 #sbt stitcher/"runMain ncats.stitcher.tools.CompoundStitcher $db 1"
 #sbt stitcher/"runMain ncats.stitcher.calculators.EventCalculator $db 1"
