ec839bfacc34be749379435713b008ff3ef76949
Ivan Grishagin <grishagin@gmail.com>
2021-10-01 17:36:38 -0400

    Added (1) test GSRS dump, (2) test FRDB dump, (3) script to make test GSRS dump (to test individual UNIIs). Modified stitching script accordingly.

--- scripts/stitching/test/stitch-all-test.sh
+++ scripts/stitching/test/stitch-all-test.sh
@@ -9,9 +9,9 @@ curr_time=$(date +%s)
 
 echo $(date) > $log
 
-sbt stitcher/"runMain ncats.stitcher.impl.RanchoJsonEntityFactory $db \"name=FRDB, July 2021\" cache=data/hash.db ../stitcher-rawinputs/files/frdb_2021-07-29.json"
+sbt stitcher/"runMain ncats.stitcher.impl.RanchoJsonEntityFactory $db \"name=FRDB, July 2021\" cache=data/hash.db data/test/frdb_test_olaparib.json"
 echo 'rancho:' $(( ($(date +%s) - $curr_time )/60 )) 'min' >> $log
-sbt stitcher/"runMain ncats.stitcher.impl.SRSJsonEntityFactory $db \"name=G-SRS, May 2021\" cache=data/hash.db ../stitcher-rawinputs/files/dump-public-2021-05-02.gsrs"
+sbt stitcher/"runMain ncats.stitcher.impl.SRSJsonEntityFactory $db \"name=G-SRS, May 2021\" cache=data/hash.db data/test/dump-test-olaparib.gsrs"
 echo 'gsrs:' $(( ($(date +%s) - $curr_time )/60 )) 'min' >> $log
 
 #sbt stitcher/"runMain ncats.stitcher.impl.MapEntityFactory stitchvTEST.db data/ob.conf"
@@ -22,6 +22,6 @@ echo 'gsrs:' $(( ($(date +%s) - $curr_time )/60 )) 'min' >> $log
 sbt -mem 32000 stitcher/"runMain ncats.stitcher.tools.CompoundStitcher $db 1"
 echo 'Stitching:' $(( ($(date +%s) - $curr_time )/60 )) 'min' >> $log
 
-zip -r $dbzip $db
+#zip -r $dbzip $db
 
 echo $(date) >> $log
