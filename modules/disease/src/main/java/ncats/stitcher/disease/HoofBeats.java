4538203888369cb63713928f9477cde3484d8550
trung <caodac@gmail.com>
2021-10-18 21:21:22 -0400

    update GTR

--- modules/disease/src/main/java/ncats/stitcher/disease/HoofBeats.java
+++ modules/disease/src/main/java/ncats/stitcher/disease/HoofBeats.java
@@ -1451,26 +1451,50 @@ public class HoofBeats {
 
         JsonNode doDiagnoses () {
             ArrayNode diagnoses = mapper.createArrayNode();
-            Entity[] ents = get ("S_MEDGEN");
-            if (ents != null && ents.length > 0) {
-                for (Entity e : ents) {
-                    boolean hasGTR = false;
-                    for (Object source : Util.toArray(e.payload("SOURCES"))) {
-                        if ("GTR".equals(source)) {
-                            hasGTR = true;
-                            break;
+
+            if (false) {
+                Entity[] ents = get ("S_MEDGEN");
+                if (ents != null && ents.length > 0) {
+                    for (Entity e : ents) {
+                        boolean hasGTR = false;
+                        for (Object source :
+                                 Util.toArray(e.payload("SOURCES"))) {
+                            if ("GTR".equals(source)) {
+                                hasGTR = true;
+                                break;
+                            }
                         }
-                    }
-                    
-                    if (hasGTR) {
-                        ObjectNode node = newJsonObject ();
-                        node.put("type", "GTR");
-                        node.put("curie", "MEDGEN:"+e.payload("CUI"));
-                        diagnoses.add(node);
+                        
+                        if (hasGTR) {
+                            ObjectNode node = newJsonObject ();
+                            node.put("type", "GTR");
+                            node.put("curie", "MEDGEN:"+e.payload("CUI"));
+                            diagnoses.add(node);
+                        }
                     }
                 }
             }
-
+            
+            Entity[] ents = get ("S_GTR");
+            if (ents != null && ents.length > 0) {
+                final Set<String> medgen = new TreeSet<>();
+                for (Entity e : ents) {
+                    e.neighbors((id, xe, key, reversed, props) -> {
+                            if (xe.is("S_MEDGEN")) {
+                                medgen.add("MEDGEN:"+xe.payload("CUI"));
+                            }
+                            return true;
+                        }, I_CODE);
+                }
+                
+                for (String id : medgen) {
+                    ObjectNode node = newJsonObject ();
+                    node.put("type", "GTR");
+                    node.put("curie", id);
+                    diagnoses.add(node);
+                }
+            }
+            
             Set<Entity> newborn = new HashSet<>();
             for (Entity[] ez : entities.values()) {
                 for (Entity e : ez) {
